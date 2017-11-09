04bd967c2016a05073987f0320d1b00d45075fae
ezequielmapel <ezequiel.mapelli@hotmail.com>
2017-11-09 11:56:43 -0200

    Controle Orbital

--- js/mainsJs.js
+++ js/mainsJs.js
@@ -1,4 +1,11 @@
-function initWorld(){
+function initWorld() {
+
+  /*  FOR DRAG
+  var bodyId = document.getElementById('bodyId');
+  bodyId.addEventListener('mousedown', function(){console.log('clicando');});
+  bodyId.addEventListener('mouseup', function(){console.log('n-clicando');});
+  */
+
   var algo;
   var width = window.innerWidth;
   var height = window.innerHeight;
@@ -6,12 +13,14 @@ function initWorld(){
   var camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 1000);
   var renderer = new THREE.WebGLRenderer({antialias:true});
   var scene = new THREE.Scene();
+  var controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.addEventListener('change', render);
 
   renderer.setSize(width, height);
   document.body.appendChild(renderer.domElement);
 
-  camera.position.y = height/2;
-  camera.position.z = 500;
+
+  camera.position.z = 650;
 
   var pointLight = new THREE.PointLight(0xFFFFFF);
 	pointLight.position.set(0,300,600);
@@ -36,10 +45,11 @@ function initWorld(){
   var worldGeo = new THREE.SphereGeometry(100,100,100);
   var materials = new THREE.MeshLambertMaterial({map: loadTexture()});
   var world = new THREE.Mesh(worldGeo, materials);
+
   scene.add(world);
-  world.position.y = 500;
   camera.lookAt(world.position);
-  
+
+
   var pointLight = new THREE.PointLight(0xFFFFFF);
   pointLight.position.set(0, 1200,600);
   scene.add(pointLight);
@@ -47,8 +57,8 @@ function initWorld(){
   function render(){
     renderer.render(scene, camera);
     requestAnimationFrame(render);
-    
     world.rotation.y +=0.001;
+    //controls.update();
 
     if (algo){
       algo.rotation.y +=0.01;
@@ -64,6 +74,47 @@ function initWorld(){
     });
     return image;
   }
+/*
+  var isDragging = false;
+  var previousMousePosition = {
+      x: 0,
+      y: 0
+  };
+  $(renderer.domElement).on('mousedown', function(e) {
+      isDragging = true;
+  })
+  .on('mousemove', function(e) {
+      //console.log(e);
+      var deltaMove = {
+          x: e.offsetX-previousMousePosition.x,
+          y: e.offsetY-previousMousePosition.y
+      };
+
+      if(isDragging) {
+
+          var deltaRotationQuaternion = new THREE.Quaternion()
+              .setFromEuler(new THREE.Euler(
+                  toRadians(deltaMove.y * 1),
+                  toRadians(deltaMove.x * 1),
+                  0,
+                  'XYZ'
+              ));
+
+          world.quaternion.multiplyQuaternions(deltaRotationQuaternion, world.quaternion);
+      }
+
+      previousMousePosition = {
+          x: e.offsetX,
+          y: e.offsetY
+      };
+    });
+
+
+    $(document).on('mouseup', function(e) {
+      isDragging = false
+    });
+
+*/
 
 
 }
